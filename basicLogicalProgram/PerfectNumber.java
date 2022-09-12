21ed3f443bb869a70cf13e1f246b49cf8ea437d6
Himanshu8922 <himanshurai9211@gmail.com>
2022-09-12 12:48:27 +0530

    basic logical program

---
+++ basicLogicalProgram/PerfectNumber.java
@@ -0,0 +1,24 @@
+package basicLogicalProgram;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+	
+public static void main(String[] args) {
+		
+		int sum=0;
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("enter the number");
+		int number = sc.nextInt();
+		for(int i=1; i<number; i++) {
+			if(number%i == 0) {
+				sum+=i;
+			}
+		}
+		
+		if(sum==number) {
+			System.out.println(number+ " is a Perfect Number");
+		}
+	}
+
+}
---
+++ basicLogicalProgram/PrimeNumber.java
@@ -0,0 +1,28 @@
+package basicLogicalProgram;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	
+	
+	
+	public static void main(String[] args) {
+
+		boolean flag = true;
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("enter the number");
+		int number = sc.nextInt();
+		    for (int i = 2; i <= number / 2; ++i) {
+		      if (number % i == 0) {
+		        flag = false;
+		        break;
+		      }
+		    }
+
+		    if (flag)
+		      System.out.println(number + " is a prime number.");
+		    else
+		      System.out.println(number + " is not a prime number.");
+  }
+
+}
